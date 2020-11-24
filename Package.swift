362caed7002aca9c7695fa7c9e7331a42b652aba
Adrian Zimmermann <zadrian@web.de>
2020-11-25 00:51:15 +0100

    update Package, link gl

--- Package.swift
+++ Package.swift
@@ -1,13 +1,21 @@
-// swift-tools-version:4.0
-
+// swift-tools-version:5.0
 import PackageDescription
 
 let package = Package(
-    name:           "GL",
-    products:      [.library(name: "GL", targets: ["GL"]),
-                    .executable(name: "generator", targets: ["generator"])],
-    dependencies:  [.package(url: "https://github.com/kelvin13/swiftxml", .branch("master"))],
-    targets:       [.target(name: "GL"        , dependencies: []      , path: "sources/opengl"),
-                    .target(name: "generator" , dependencies: ["XML"] , path: "sources/generator")],
-    swiftLanguageVersions: [4]
+    name: "GL",
+    products: [
+        .library(name: "GL", targets: ["GL"]),
+        .executable(name: "generator", targets: ["generator"])],
+    dependencies:  [
+        .package(url: "https://github.com/kelvin13/swiftxml", .branch("master"))],
+    targets:       [
+        .target(
+            name: "GL",
+            dependencies: [],
+            path: "sources/opengl",
+            linkerSettings: [.linkedFramework("gl")]),
+        .target(
+            name: "generator",
+            dependencies: ["XML"],
+            path: "sources/generator")]
 )
